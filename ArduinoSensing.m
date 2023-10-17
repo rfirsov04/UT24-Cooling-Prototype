7cd3ea24fbcc0a6b975d73ea13687bad1f24e4f8
rfirsov04 <90947016+rfirsov04@users.noreply.github.com>
2023-10-17 11:24:12 -0400

    Add files via upload
    Fixed time logging and quit function to rely only on a certain key

--- ArduinoSensing.m
+++ ArduinoSensing.m
@@ -1,6 +1,5 @@
 clear
 a = arduino('COM10', 'Uno');
-
 i = 0;
 v1 = readVoltage(a, 'A0')  %Analog input
 v2 = readVoltage(a, 'A1')
@@ -8,18 +7,20 @@ v3 = readVoltage(a, 'A2')
 v4 = readVoltage(a, 'A3')
 v5 = readVoltage(a, 'A4')
 v6 = readVoltage(a, 'A5')
+set(gcf,'CurrentCharacter', '@');
 flag = get(gcf,'CurrentCharacter');
 
 %my_structure.time = zeros(3600, 1); if logging faster than once per second
-my_structure.voltageA0 = zeros(3600, 1);
-my_structure.voltageA1 = zeros(3600, 1);
-my_structure.voltageA2 = zeros(3600, 1);
-my_structure.voltageA3 = zeros(3600, 1);
-my_structure.voltageA4 = zeros(3600, 1);
-my_structure.voltageA5 = zeros(3600, 1);
+my_structure.voltageA0 = zeros(4000, 1);
+my_structure.voltageA1 = zeros(4000, 1);
+my_structure.voltageA2 = zeros(4000, 1);
+my_structure.voltageA3 = zeros(4000, 1);
+my_structure.voltageA4 = zeros(4000, 1);
+my_structure.voltageA5 = zeros(4000, 1);
 
 ax = axes();
 hold on;
+
 line1 = line(i, v1);  %handle for line 1s
 line2 = line(i, v2);
 line3 = line(i, v3);
@@ -27,16 +28,17 @@ line4 = line(i, v4);
 line5 = line(i, v5);
 line6 = line(i, v6);
 
-axis([0 inf 1.3 2.1]) %Chose those y-axis values because in normal conditions, temp is aprox 22ºC
+axis([0 inf 1.4 2.1]) %Chose those y-axis values because in normal conditions, temp is aprox 22ºC
 i = i + 1;
-while(isempty(flag) == 1) %For making the loop infinite
+while(flag ~= '\') %For making the loop infinite
+  tic
   v1=readVoltage(a, 'A0');  %Analog input for the first NTC temperature sensor
   v2=readVoltage(a, 'A1');
   v3=readVoltage(a, 'A2');
   v4=readVoltage(a, 'A3');
   v5=readVoltage(a, 'A4');
   v6=readVoltage(a, 'A5');
-  pause(1.0);        
+         
   line1.XData = [line1.XData i];
   line1.YData = [line1.YData v1];
   hold on
@@ -60,6 +62,7 @@ while(isempty(flag) == 1) %For making the loop infinite
   my_structure.voltageA5(i) = v6;
 
   flag = get(gcf,'CurrentCharacter');
+  pause(1.0 - toc);
 end
 
-save tempzener200_2.mat -struct my_structure %or just save it every iteration of the while loop so we don't need flag which delays by like 10 seconds
+save tempzener15A4Soutlet0.mat -struct my_structure %or just save it every iteration of the while loop so we don't need flag which delays by like 10 seconds
